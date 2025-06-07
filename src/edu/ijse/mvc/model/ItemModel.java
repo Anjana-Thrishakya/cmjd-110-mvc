1eab992b48e6afc56eb0e2146d3bd28915a1d9d8
Anjana Thrishakya <anjanathrishakya@gmail.com>
2025-06-07 12:19:51 +0530

    ItemModel Complete

--- src/edu/ijse/mvc/model/ItemModel.java
+++ src/edu/ijse/mvc/model/ItemModel.java
@@ -8,6 +8,8 @@ import edu.ijse.mvc.db.DBConnection;
 import edu.ijse.mvc.dto.ItemDto;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.ArrayList;
 
 /**
  *
@@ -25,5 +27,68 @@ public class ItemModel {
         statement.setInt(5, itemDto.getQoh());
         
         return  statement.executeUpdate() > 0 ? "Success" : "Fail";
+    }
+    
+    public String updateItem(ItemDto itemDto) throws Exception{
+        Connection connection = DBConnection.getInstance().getConnection();
+        String sql = "UPDATE Item SET Description = ?, PackSize = ?, UnitPrice = ?, QtyOnHand = ? WHERE ItemCode = ?";
+        PreparedStatement statement = connection.prepareStatement(sql);
+        statement.setString(1, itemDto.getDesc());
+        statement.setString(2, itemDto.getPack());
+        statement.setDouble(3, itemDto.getUnitPrice());
+        statement.setInt(4, itemDto.getQoh());
+        statement.setString(5, itemDto.getCode());
+        
+        return  statement.executeUpdate() > 0 ? "Success" : "Fail";
+    }
+    
+    public String deleteItem(String itemCode) throws Exception{
+        Connection connection = DBConnection.getInstance().getConnection();
+        String sql = "DELETE FROM Item WHERE ItemCode = ?";
+        PreparedStatement statement = connection.prepareStatement(sql);
+        statement.setString(1, itemCode);
+
+        return  statement.executeUpdate() > 0 ? "Success" : "Fail";
+    }
+    
+    public ItemDto getItem(String itemCode) throws Exception{
+        Connection connection = DBConnection.getInstance().getConnection();
+        String sql = "SELECT * FROM Item WHERE ItemCode = ?";
+        PreparedStatement statement = connection.prepareStatement(sql);
+        statement.setString(1, itemCode);
+        
+        ResultSet rst = statement.executeQuery();
+        
+        if(rst.next()){
+            ItemDto itemDto = new ItemDto(
+                    rst.getString("ItemCode"),
+                    rst.getString("Description"), 
+                    rst.getString("PackSize"),
+                    rst.getDouble("UnitPrice"),
+                    rst.getInt("QtyOnHand"));
+        
+            return itemDto;
+        }
+        return null;
+    }
+    
+    public ArrayList<ItemDto> getAll() throws Exception{
+        Connection connection = DBConnection.getInstance().getConnection();
+        String sql = "SELECT * FROM Item";
+        PreparedStatement statement = connection.prepareStatement(sql);
+        
+        ResultSet rst = statement.executeQuery();
+        ArrayList<ItemDto> itemDtos = new ArrayList<>();
+        while(rst.next()){
+            ItemDto itemDto = new ItemDto(
+                    rst.getString("ItemCode"),
+                    rst.getString("Description"), 
+                    rst.getString("PackSize"),
+                    rst.getDouble("UnitPrice"),
+                    rst.getInt("QtyOnHand"));
+        
+            itemDtos.add(itemDto);
+        }
+        return itemDtos;
     }
 }
